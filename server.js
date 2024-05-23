730ba9423288d648ff3532c040e48223493e1cbe
AndreaGul <andreagulino99@gmail.com>
2024-05-23 18:40:06 +0200

    feat:ricezione chiamata api in console

--- server.js
+++ server.js
@@ -4,12 +4,46 @@ const host = process.env.HOST || 'localhost';
 const fs = require('fs');
 const path = require('path');
 
+const DB_FILE = 'norrisDb';
+
+const readJSONData = (nomeFile) => {
+  const filePath = path.join(__dirname, nomeFile + '.json');
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    if (fileData) {
+      return JSON.parse(fileData);
+    }
+  }
+  return [];
+};
+
+const writeJSONData = (nomeFile, newData) => {
+  const filePath = path.join(__dirname, nomeFile + '.json');
+  const fileString = JSON.stringify(newData);
+  fs.writeFileSync(filePath, fileString);
+};
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { Content_Type: 'text/html; charset=utf-8' });
-  fetch('https://api.chucknorris.io/jokes/random').then((response) =>
-    response.json().then()
-  );
-  res.end('<> ciao</>');
+  const norrisDb = readJSONData('norrisDb');
+  switch (req.url) {
+    case '/favicon.ico':
+      res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.end();
+      break;
+    case '/':
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      //test con API
+      fetch('https://fakestoreapi.com/users')
+        .then((response) => response.json())
+        .then((users) => {
+          console.log(users);
+          res.end();
+        });
+      break;
+    default:
+      res.end();
+      break;
+  }
 });
 
 server.listen(port, host, () => {
